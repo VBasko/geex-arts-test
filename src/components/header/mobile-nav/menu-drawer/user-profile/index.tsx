8b7798e8dfda259a561d178614d314e85c25f49f
VBasko <vvbasko@outlook.com>
2024-06-18 03:59:41 -0300

    fix: imports

--- src/components/header/mobile-nav/menu-drawer/user-profile/index.tsx
+++ src/components/header/mobile-nav/menu-drawer/user-profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAuth } from "@/api/auth-context";
 import {
   ListItem,
--- src/components/ui/icons/outline-eye-icon.tsx
+++ src/components/ui/icons/outline-eye-icon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const OutlineEyeIcon = () => {
   return (
     <svg
